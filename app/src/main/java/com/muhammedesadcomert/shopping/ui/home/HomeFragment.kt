7684967bab908ee1c27a51014fc657ba84af60db
Muhammed Esad Cömert <esadcmrt@gmail.com>
2022-09-09 10:30:02 +0300

    Fix crash when clicking on a category
    (cherry picked from commit b1d65d05fd870fa7fb8b208974a540e925b9611a)

--- app/src/main/java/com/muhammedesadcomert/shopping/ui/home/HomeFragment.kt
+++ app/src/main/java/com/muhammedesadcomert/shopping/ui/home/HomeFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.muhammedesadcomert.shopping.databinding.FragmentHomeBinding
 import com.muhammedesadcomert.shopping.ui.home.model.Category
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,7 +41,7 @@ class HomeFragment : Fragment() {
 
     private fun initCategoryAdapter() {
         val categoryAdapter = CategoryAdapter(categories) { category ->
-            TODO()
+
         }
 
         binding.recyclerViewCategories.apply {
