c098903f934ba471aefb6c51f06c9cfc1632a671
Muhammed Esad Cömert <esadcmrt@gmail.com>
2022-09-10 20:04:51 +0300

    Ádd product UI model

---
+++ app/src/main/java/com/muhammedesadcomert/shopping/domain/model/Product.kt
@@ -0,0 +1,8 @@
+package com.muhammedesadcomert.shopping.domain.model
+
+data class Product(
+    val title: String? = null,
+    val image: String? = null,
+    val price: Double? = null,
+    val campaignPrice: Double? = null,
+)
