2fe0cd0823450bfa52a40371513ea4c24154e185
Muhammed Esad Cömert <esadcmrt@gmail.com>
2022-09-11 14:37:24 +0300

    Show shimmer effect while loading details

--- app/src/main/java/com/muhammedesadcomert/shopping/common/util/extension/ShimmerExt.kt
+++ app/src/main/java/com/muhammedesadcomert/shopping/common/util/extension/ShimmerExt.kt
@@ -1,18 +1,17 @@
 package com.muhammedesadcomert.shopping.common.util.extension
 
-import android.view.View
 import com.facebook.shimmer.ShimmerFrameLayout
 
 fun ShimmerFrameLayout.startShimmerLayout() {
     if (this.isShimmerVisible.not()) {
         this.showShimmer(true)
-        this.visibility = View.VISIBLE
+        this.visible()
     }
 }
 
 fun ShimmerFrameLayout.stopShimmerLayout() {
     if (this.isShimmerVisible && this.isShimmerStarted) {
         this.hideShimmer()
-        this.visibility = View.GONE
+        this.gone()
     }
 }
